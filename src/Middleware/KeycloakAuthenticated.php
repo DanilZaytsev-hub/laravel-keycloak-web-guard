1989fbe09ae693b0fb8edb6a748a4fd362a798e9
DanilZaytsev-hub <d.zaicev@danaflex.ru>
2022-07-26 15:02:23 +0300

    Update KeycloakAuthenticated.php

--- src/Middleware/KeycloakAuthenticated.php
+++ src/Middleware/KeycloakAuthenticated.php
@@ -14,6 +14,6 @@ class KeycloakAuthenticated extends Authenticate
      */
     protected function redirectTo($request)
     {
-        return route(url(config('keycloak-web.redirect_url')));
+        return url(config('keycloak-web.redirect_url'));
     }
 }
