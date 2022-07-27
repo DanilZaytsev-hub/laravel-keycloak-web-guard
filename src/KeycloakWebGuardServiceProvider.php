f6d882b79db3780548a66d171505fd1301f047cb
DanilZaytsev-hub <d.zaicev@danaflex.ru>
2022-07-27 13:20:56 +0300

    routes

--- src/KeycloakWebGuardServiceProvider.php
+++ src/KeycloakWebGuardServiceProvider.php
@@ -4,17 +4,12 @@ namespace Vizir\KeycloakWebGuard;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Vizir\KeycloakWebGuard\Auth\Guard\KeycloakWebGuard;
 use Vizir\KeycloakWebGuard\Auth\KeycloakWebUserProvider;
-use Vizir\KeycloakWebGuard\Middleware\KeycloakAuthenticated;
-use Vizir\KeycloakWebGuard\Middleware\KeycloakCan;
-use Vizir\KeycloakWebGuard\Middleware\KeycloakHas;
-use Vizir\KeycloakWebGuard\Models\KeycloakUser;
 use Vizir\KeycloakWebGuard\Services\KeycloakService;
 
 class KeycloakWebGuardServiceProvider extends ServiceProvider
@@ -38,11 +33,11 @@ class KeycloakWebGuardServiceProvider extends ServiceProvider
         });
 
         // Gates
-        Gate::define('keycloak-web', function ($user, $roles, $resource = '') {
+        Gate::define('keycloak-web-can', function ($user, $roles, $resource = '') {
             return $user->hasRole($roles, $resource) ?: null;
         });
 
-        Gate::define('keycloak-web-permission', function ($user, $permissions, $resource = '') {
+        Gate::define('keycloak-web-has', function ($user, $permissions, $resource = '') {
             return $user->hasPermission($permissions, $resource) ?: null;
         });
     }
@@ -68,58 +63,9 @@ class KeycloakWebGuardServiceProvider extends ServiceProvider
         // Routes
         $this->registerRoutes();
 
-        // Middleware Group
-        // $this->app['router']->middlewareGroup('keycloak-web', [
-        //     StartSession::class,
-        //     KeycloakAuthenticated::class,
-        // ]);
-
-        // Add Middleware "keycloak-web-can"
-        // $this->app['router']->aliasMiddleware('keycloak-web-can', KeycloakCan::class);
-
-        // Add Middleware "keycloak-web-has"
-        // $this->app['router']->aliasMiddleware('keycloak-web-has', KeycloakHas::class);
-
         // Bind for client data
         $this->app->when(KeycloakService::class)->needs(ClientInterface::class)->give(function() {
             return new Client(Config::get('keycloak-web.guzzle_options', []));
         });
-    }
-
-    /**
-     * Register the authentication routes for keycloak.
-     *
-     * @return void
-     */
-    private function registerRoutes()
-    {
-        $defaults = [
-            'login' => 'login',
-            'logout' => 'logout',
-            'register' => 'register',
-            'callback' => 'callback',
-        ];
-
-        $routes = Config::get('keycloak-web.routes', []);
-        $routes = array_merge($defaults, $routes);
-
-        // Register Routes
-        $router = $this->app->make('router');
-
-        if (! empty($routes['login'])) {
-            $router->middleware('web')->get($routes['login'], 'Vizir\KeycloakWebGuard\Controllers\AuthController@login')->name('keycloak.login');
-        }
-
-        if (! empty($routes['logout'])) {
-            $router->middleware('web')->get($routes['logout'], 'Vizir\KeycloakWebGuard\Controllers\AuthController@logout')->name('keycloak.logout');
-        }
-
-        if (! empty($routes['register'])) {
-            $router->middleware('web')->get($routes['register'], 'Vizir\KeycloakWebGuard\Controllers\AuthController@register')->name('keycloak.register');
-        }
-
-        if (! empty($routes['callback'])) {
-            $router->middleware('web')->get($routes['callback'], 'Vizir\KeycloakWebGuard\Controllers\AuthController@callback')->name('keycloak.callback');
-        }
     }
 }
