7a3472117da2766260287982ddc2f8d1bff842af
DanilZaytsev-hub <d.zaicev@danaflex.ru>
2022-07-28 11:30:14 +0300

    Revert "fix"
    This reverts commit fed58998ab08bec863fa9250d6db2f9116d8c382.

--- src/Auth/Guard/KeycloakWebGuard.php
+++ src/Auth/Guard/KeycloakWebGuard.php
@@ -140,11 +140,6 @@ class KeycloakWebGuard implements Guard
         $user = $this->provider->retrieveByCredentials([
             'email' => $user['email'],
         ]);
-
-        if (!$user) {
-            return false;
-        }
-        
         $this->setUser($user);
 
         return true;
